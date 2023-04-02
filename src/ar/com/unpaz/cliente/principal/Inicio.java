4597dbf6a9af17c218008df0220bc12bfa922555
gonzafranco <gonzafrancoandres@gmail.com>
2023-04-02 18:24:38 -0300

    Add pattern

--- src/ar/com/unpaz/cliente/principal/Inicio.java
+++ src/ar/com/unpaz/cliente/principal/Inicio.java
@@ -1,6 +1,7 @@
 package ar.com.unpaz.cliente.principal;
 
 import java.util.List;
+import java.util.Scanner;
 
 import ar.com.unpaz.cliente.repositoryImp.LecturaRepositoryImp;
 import ar.com.unpaz.cliente.serviceImp.LecturaServiceImp;
@@ -14,10 +15,30 @@ public class Inicio {
 
 		LecturaServiceImp lectura = new LecturaServiceImp();	
 		buscarServiceImp buscar = new buscarServiceImp();
+		List datos = guardarDatos.leer("C:\\Users\\Vanilla\\Downloads\\cabecera.txt");
+		int op = 0;
+		Scanner input = new Scanner(System.in);
 		
-		List datos = guardarDatos.leer("C:\\Users\\Aula Multimedia\\Downloads\\cabecera.txt");
-		
-		buscar.buscarCliente(datos);
+		do {
+			
+			System.out.println("--------------");
+			System.out.println("Menu Principal");
+			System.out.println("--------------");
+			System.out.println("1 - Buscar cliente");
+			System.out.println("2 - Cliente");
+			System.out.println("0 - Salir");
+			op = input.nextInt();
+			
+			switch(op) {
+			 case 1:
+				 	buscar.buscarCliente(datos);
+	                break;
+			 case 2:
+				 	System.out.println("Ventas realizadas: " + datos.size());
+	                break;
+			}
+			
+		}while (op != 0);
 		
 		//lectura.leer(datos);
 
--- src/ar/com/unpaz/cliente/serviceImp/buscarServiceImp.java
+++ src/ar/com/unpaz/cliente/serviceImp/buscarServiceImp.java
@@ -2,6 +2,8 @@ package ar.com.unpaz.cliente.serviceImp;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import ar.com.unpaz.cliente.model.cliente;
 import ar.com.unpaz.cliente.service.buscarService;
@@ -10,26 +12,24 @@ public class buscarServiceImp implements buscarService {
 	
 	@Override
 	public void buscarCliente(List<cliente> c) {
-		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
-		String nom;
-		
-	
-			System.out.println("Ingrese un nombre: ");
-			 nom = input.nextLine();
-			
-		
-		
-		c.forEach(cliente -> {
-				
-				if(nom.equals(cliente.getNombreApellido())) {
-					System.out.println(cliente.toString());
-					
-					System.out.println(System.getProperty("user.dir"));
+	    Scanner input = new Scanner(System.in);
+	    String nom;
+	    
+	    do {
+	        System.out.println("Ingrese un nombre: ");
+	        nom = input.nextLine();
+	    } while(nom.length() < 4);
 
-				}
-			}	
-		);
+	    Pattern pattern = Pattern.compile(nom, Pattern.CASE_INSENSITIVE);
+
+	    c.forEach(cliente -> {
+	        Matcher matcher = pattern.matcher(cliente.getNombreApellido());
+	        
+	        if(matcher.find()) {
+	            System.out.println(cliente.toString());
+	            System.out.println(System.getProperty("user.dir"));
+	        }
+	    });
+
 	}
-
 }
